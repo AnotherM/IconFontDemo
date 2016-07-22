eb5a6f25c806fc6ab26562dee3251d7cf88bb9ca
AnotherM <zm6152@gmail.com>
2016-07-22 16:42:36 +0800

    Initial commit

---
+++ app/src/main/java/m/another/iconfont/MainActivity.java
@@ -0,0 +1,17 @@
+package m.another.iconfont;
+
+import android.os.Bundle;
+import android.support.v4.view.LayoutInflaterCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mikepenz.iconics.context.IconicsLayoutInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        LayoutInflaterCompat.setFactory(getLayoutInflater(), new IconicsLayoutInflater(getDelegate()));
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
